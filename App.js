d78708c7004794c4b7d6486b372d8afa46c790c1
Guilherme Campos <guifercampoficial@gmail.com>
2025-10-01 17:51:39 -0300

    adicionadas as transicoes para as telas de menu do administrador e lista de alunos

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import LoginScreen from './screens/screen-Login';
 import SignupScreen from './screens/screen-Signup';
+import MenuAdmin from './screens/screen-AdmMenu';
+import MenuADM_Alunos from './screens/screen-AdmListaAlunos';
 
 const Stack = createNativeStackNavigator();
 
@@ -17,7 +19,13 @@ export default function App() {
         
         {/*tela cadastro*/}
         <Stack.Screen name="Signup" component={SignupScreen} options={{ title: 'Criar Conta'}}/>
+
+        {/*tela menu do administrador*/}
+        <Stack.Screen name="MenuAdmin" component={MenuAdmin} options={{ title: 'Menu do Administrador'}}/>
       
+        {/*tela lista de alunos do administrador*/}
+        <Stack.Screen name="Alunos" component={MenuADM_Alunos} options={{ title: 'Lista de Alunos'}}/>
+
       </Stack.Navigator>
     </NavigationContainer>
   );
